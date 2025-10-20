d262ee04c38b518456d60e28107e65b8bb53f356
hexaliokung <thatphongsaikham@gmail.com>
2025-10-21 01:09:38 +0700

    LearnJsx

---
+++ src/components/Hello.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hello = () => {
+  /*
+  return (
+    <div id = 1, className='dummyClass'>
+      <h1>Hello Vishwas</h1>
+    </div>
+  );
+  */
+  return React.createElement(
+    "div",
+    { id: "hello", className: "dummyClass" },
+    React.createElement("h1", null, "Hello Vishwas")
+  );
+};
+
+export default Hello;
